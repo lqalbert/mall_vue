3bdf5d8061b82a43d9bc6d2f06063e4486e7e4b0
hexiaojiao <laurel_he@163.com>
2018-01-04 16:06:16 +0800

    修改商品详情页

--- src/components/Category/Index.vue
+++ src/components/Category/Index.vue
@@ -2,32 +2,55 @@
     <!--<el-tree :data="data" :props="defaultProps" ></el-tree>-->
     <div>
 
-        <el-input v-model="type_name" placeholder="请输入要添加的商品类名" style="width: 300px"></el-input>
+        <el-input v-model="type_name" placeholder="输入关键字进行过滤" style="width: 300px"></el-input>
         <el-tree
+                class="filter-tree"
                 :data="data"
                 :props="defaultProps"
                 show-checkbox
                 node-key="id"
                 :expand-on-click-node="false"
+                :filter-node-method="filterNode"
+                ref="tree2"
                 :render-content="renderContent">
         </el-tree>
+        <el-button type="primary" @click="editCont">添加分类</el-button>
+
+        <addDialog :add-open="addDialog" @add-window-close="handleAddWindow" @aaa='bbb'/>
+<editDialog :edit-open="editDialog" @add-window-close="handleAddWindow" @add='addAll'/>
+
     </div>
 
 </template>
 
 <script>
+    import addDialog from './addDialog';
+    import editDialog from './editDialog';
     import PageMix from '../../mix/Page';
     import DataProxy from '../../packages/DataProxy';
     import SearchTool from '../../mix/SearchTool';
     let id = 1000;
         export default {
-
+            watch: {
+              type_name(val) {
+                this.$refs.tree2.filter(val);
+              }
+            },
             name: 'Category',
             pageTitle:"商品分类",
+            components: {
+                    addDialog,
+                    editDialog,
+                },
             mixins: [PageMix, SearchTool],
             data() {
                 return {
+                    addDialog:false,
+                    editDialog:false,
+
+                    dev:'',
                     type_name:'',
+                    data1:'',
                     data: [{
                         id:1,
                         label: '化妆品',
@@ -76,28 +99,93 @@
                 },
 
             methods: {
-                append(store, data) {
-                    store.append({ id: id++, label: this.type_name, children: [] }, data);
-                    this.type_name='';
-                },
+            bbb:function(v){
+             this.append(this.dev,this.data1,v.name);
+             console.log(this.data1);
+            },
+            addAll:function(v){
+                var data = this.data;
+                var res = data[data.length-1];
+                var newData=[];
+                 newData.id = res.id;
 
-                remove(store, data) {
-                    store.remove(data);
-                },
+                newData.children=[];
+                newData.label = v.name;
+                this.data.push(newData);
+                console.log(this.data);
 
-                renderContent(h, { node, data, store }) {
 
-                    return (
-                        <span>
-                        <span>
-                        <span>{node.label}</span>
-                       </span>
-                    <span style="float: right; margin-right: 20px">
-                        <el-button size="mini" type="info" on-click={ () => this.append(store, data) }>添加分类</el-button>
-                        <el-button size="mini" type="danger" on-click={ () => this.remove(store, data) }>删除分类</el-button>
-                    </span>
-                    </span>);
-                }
+            },
+            test:function(store, data){
+                this.dev=store;
+                this.data1 = data;
+                this.addDialog=true;
+            },
+            editCont:function(){
+                this.editDialog=true;
+            },
+            app(store,data,v)
+            {
+                this.$prompt('请输入分类名称', '提示', {
+                          confirmButtonText: '确定',
+                          cancelButtonText: '取消',
+                        }).then(({ value }) => {
+                        this.append(store,data,value),
+                          this.$message({
+                            type: 'success',
+                            message: '你的分类名称是: ' + value
+                          });
+                        }).catch(() => {
+                          this.$message({
+                            type: 'info',
+                            message: '取消输入'
+                          });
+                        });
+
+            },
+            handleedit:function(){
+                this.editDialog =true;
+
+            },
+            switchHandle:function(index,row){
+
+            },
+            openAddDialog:function(){
+
+            },
+            closeDialog:function(){
+
+            },
+            handleAddWindow(){
+                this.addDialog = false;
+                this.editDialog =false;
+            },
+            append(store, data,v) {
+                store.append({ id: id++, label: v, children: [] }, data);
+                this.type_name='';
+            },
+            filterNode(value, data) {
+                    if (!value) return true;
+                    return data.label.indexOf(value) !== -1;
+                  },
+
+            remove(store, data) {
+                store.remove(data);
+            },
+
+            renderContent(h, { node, data, store }) {
+
+                return (
+                    <span>
+                    <span>
+                    <span>{node.label}</span>
+                   </span>
+                <span style="float: right; margin-right: 20px">
+                    <el-button size="mini" type="info"   on-click={ () => this.test(store, data) }>添加分类</el-button>
+                    <el-button size="mini" type="danger" on-click={ () => this.remove(store, data) }>删除分类</el-button>
+                </span>
+                </span>);
+            }
             }
 
 
---
+++ src/components/Category/addDialog.vue
@@ -0,0 +1,76 @@
+<template>
+    <div >
+        <el-dialog title="添加" :visible.sync="state7" @close="handleClose">
+            <el-form :model="addForm" :inline="true" ref="addForm"  labelPosition="right"
+            labelWidth='100px'>
+                <el-row>
+                    <el-col :span="24">
+                        <el-form-item label="分类名称" prop="name">
+                            <el-input class="name-input" v-model="addForm.name"  auto-complete="off" placeholder="请填写分类名称"></el-input>
+                        </el-form-item>
+                    </el-col>
+
+                </el-row>
+
+            </el-form>
+
+
+
+            <div slot="footer" class="dialog-footer">
+                <el-button @click="closeDialog()">取 消</el-button>
+                <el-button @click="addFormSubmit()" type="primary"  >确 定</el-button>
+            </div>
+        </el-dialog>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'addDialog',
+        props:{
+            addOpen:{
+                type:Boolean,
+                default:false
+            }
+        },
+        data () {
+            return {
+
+                state7: this.addOpen,
+                addForm:{
+                    name:"",
+
+                },
+
+            }
+        },
+
+        methods:{
+            handleClose(){
+                this.$emit('add-window-close');
+            },
+            addFormSubmit:function(){
+//                console.log(this.addForm);
+                this.$emit('aaa',{name:this.addForm.name});
+                this.addForm.name='';
+                this.state7=false;
+            },
+            closeDialog:function(){
+                this.state7=false;
+            }
+        },
+        watch:{
+            addOpen:function(val, oldVal){
+                this.state7 = val;
+            }
+        }
+    }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .name-input{
+        max-width: 217px;
+    }
+</style>
+

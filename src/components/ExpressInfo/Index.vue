e4befe78edfc31c436b05b4c95559f193aec6917
hujianjian <hujian_gosu@163.com>
2018-01-17 14:06:07 +0800

    20180117编辑器上传图片

--- src/components/ExpressInfo/Index.vue
+++ src/components/ExpressInfo/Index.vue
@@ -82,11 +82,16 @@
     import DataTable from '../../mix/DataTable';
     import SelectProxy from  '../../packages/SelectProxy';
     import ExpressInfoAjaxProxy from '../../ajaxProxy/ExpressInfo';
+    import { quillRedefine } from 'vue-quill-editor-upload';
+    import URL_CONST from '../../config';
 
     export default {
     name: 'ExpressInfo',
     pageTitle:"物流信息",
     mixins: [PageMix,SearchTool,DataTable,ExpressInfoAjaxProxy],
+    components: {
+        quillRedefine,
+    },
     data () {
         return {
             mainparam:"",
@@ -105,17 +110,7 @@
                 }
             },
             editContent:'',
-            editorOption:{
-                //debug: 'info',
-                modules: {
-                    toolbar:[['bold', 'italic','code-block',{'font':['SimSun','KaiTi','Microsoft Yahei']},
-                    { size: [ 'small', false, 'large', 'huge' ]}],['link', 'image']],
-                },
-                placeholder: '填写点什么吧',
-                //readOnly: true,
-                theme: 'snow',
-                syntax: true, 
-            }
+            editorOption:{}
 
 
         }
@@ -177,6 +172,41 @@
     created(){
         this.$on('search-tool-change', this.onSearchChange);
         this.getExpressStatus();
+        this.editorOption = quillRedefine(
+            {
+                // 图片上传的设置
+                uploadConfig: {
+                    action: URL_CONST.UPLOAD_URL,  // 必填参数 图片上传地址
+                    // 必选参数  res是一个函数，函数接收的response为上传成功时服务器返回的数据
+                    // 你必须把返回的数据中所包含的图片地址 return 回去
+                    res: (respnse) => {
+                        console.log(respnse);
+                        return respnse.data.fullurl;
+                    },
+                    methods: 'POST',  // 可选参数 图片上传方式  默认为post
+                    //token: sessionStorage.token,  // 可选参数 如果需要token验证，假设你的token有存放在sessionStorage
+                    name: 'avatar',  // 可选参数 文件的参数名 默认为img
+                    size: 500,  // 可选参数   图片限制大小，单位为Kb, 1M = 1024Kb
+                    accept: 'image/png, image/gif, image/jpeg, image/jpg',  // 可选参数 可上传的图片格式
+                    // start: function (){}
+                    start: () => {
+                    },  // 可选参数 接收一个函数 开始上传数据时会触发
+                    end: () => {
+                    },  // 可选参数 接收一个函数 上传数据完成（成功或者失败）时会触发
+                    success: () => {
+                    },  // 可选参数 接收一个函数 上传数据成功时会触发
+                    error: () => {
+                    }  // 可选参数 接收一个函数 上传数据中断时会触发
+                },
+                // 以下所有设置都和vue-quill-editor本身所对应
+                placeholder: '写点什么吧！',  // 可选参数 富文本框内的提示语
+                theme: 'snow', // 可选参数 富文本编辑器的风格
+                toolOptions: [['bold', 'italic','code-block',{'font':['SimSun','KaiTi','Microsoft Yahei']},
+                    { size: [ 'small', false, 'large', 'huge' ]}],['link', 'image']],  // 可选参数  选择工具栏的需要哪些功能  默认是全部
+                handlers: {}  // 可选参数 重定义的事件，比如link等事件
+            }
+        );
+        console.log(this.editorOption);
     }
 }
 </script>

83781d39bb0eda8f4f32a10b59f0f21f20af0b47
mcdyqnc <hyfdyx@qq.com>
2018-02-11 17:45:49 +0800

    attr un done

--- src/components/common/AttrFormItem.vue
+++ src/components/common/AttrFormItem.vue
@@ -1,15 +1,34 @@
 <template>
     <div>
         <!-- 文本 -->
-        <span v-if="type=='文本'">
+        <!-- <span v-if="type=='文本'">
             <el-input v-model="formObject.value"></el-input>
-        </span>
+        </span> -->
         <!-- 图片 -->
-        <span v-else >
+        <!-- <span v-else >
             <el-input v-model="formObject.value"></el-input>
             <el-input type="hidden" v-model="formObject.addon_value"></el-input>
             <el-button>上传</el-button>
-        </span>
+        </span> -->
+
+        <el-form-item
+            v-for="(item, index) in formObject"
+            :label="item.name"
+            :key="item.id"
+            :prop="'formObject.' + index + '.value'">   
+            <span v-if="item.type!='自选'">
+                <el-input v-model="item.value"></el-input>
+                <span v-if="item.type='图片'">
+                    <el-input type="hidden" v-model="item.addon_value"></el-input>
+                    <el-button>上传</el-button>
+                </span>
+            </span>
+            <span v-else>
+                <el-select v-model="item.value" >
+                    <el-option v-for="attr in item.add_value" :label="attr.value" :key="attr.value" :value="attr.value"></el-option>
+                </el-select>
+            </span>
+        </el-form-item>
     </div>
   </template>
   
@@ -23,7 +42,7 @@
         },
         formObject:{
             required: true,
-            type: Object
+            type: Array
         }
     },
     data () {

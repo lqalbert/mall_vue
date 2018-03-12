a1f62b0ca296c964b498df25794d8044eaf22377
yrg <huj@riign.com>
2018-03-12 11:10:23 +0800

    20180312

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -247,7 +247,7 @@
         },
         mounted:function(){
             let vThis = this;
-            this.navChange('ShelvesPick');
+            this.navChange('Workbench');
             setInterval(function(){
                 vThis.getCurDate();
             }, 1000);

02ba28e38b773331f89c419ac6488736ca851112
mcdyqnc <hyfdyx@qq.com>
2018-03-24 19:41:47 +0800

    明细未完成

--- src/components/StockSum/index.vue
+++ src/components/StockSum/index.vue
@@ -2,10 +2,11 @@
     <div>
         <el-row>
             <el-col :span="24">
-                <el-form :inline="true"  ref="searchForm" :model="searchForm" >
+                <el-form :inline="true"  ref="searchForm" :model="searchForm" class="demo-form-inline" size="small">
                     <el-form-item prop="department_id"  >
                         <el-select
-                                v-model="searchForm.distribution_id"
+                                clearable
+                                v-model="searchForm.entrepot_id"
                                 size="small"
                                 placeholder="配送中心">
                             <el-option v-for="v in distributors" :label="v.name"
@@ -14,12 +15,24 @@
                         </el-select>
                     </el-form-item>
 
-                    <el-form-item prop="product_id"  >
+                    <el-form-item prop="cate_type_id"  >
+                            <el-select
+                                    v-model="searchForm.cate_type_id"
+                                    size="small"
+                                    placeholder="商品类型"
+                                    @change="typeChange">
+                                <el-option v-for="v in types" :label="v.label"
+                                           :value="v.id" :key="v.id">
+                                </el-option>
+                            </el-select>
+                        </el-form-item>
+
+                    <el-form-item prop="cate_kind_id"  >
                         <el-select
-                                v-model="searchForm.product_id"
+                                v-model="searchForm.cate_kind_id"
                                 size="small"
                                 placeholder="商品品类">
-                            <el-option v-for="v in productNames" :label="v.name"
+                            <el-option v-for="v in cate_kinds" :label="v.label"
                                        :value="v.id" :key="v.id">
                             </el-option>
                         </el-select>
@@ -33,8 +46,9 @@
                         <el-date-picker
                             size="small"
                             v-model="searchForm.searchTime"
-                            type="datetimerange"
-                            placeholder="选择时间范围">
+                            type="daterange"
+                            placeholder="选择时间范围"
+                            @change='timeChange'>
                         </el-date-picker>
                     </el-form-item>
                     <el-form-item>
@@ -47,19 +61,17 @@
         </el-row>
         <el-row>
             <el-col :span="24">
-                <el-tabs>
-                    <el-table :data="dataList" empty-text="暂无数据" border>
-                        <el-table-column label="序号" align="center" type="index" width="65"></el-table-column>
-                        <el-table-column prop="distribution_name" label="配送中心" width="180" align="center"></el-table-column>
-                        <el-table-column prop="goods_name" label="商品名称" width="180" align="center"></el-table-column>
-                        <el-table-column prop="product_name" label="商品种类" width="180" align="center"></el-table-column>
-                        <el-table-column prop="usable_stock_number" label="可用库存数" width="180" align="center"></el-table-column>
-                        <el-table-column prop="production_out_number" label="累记出库数量" align="center"></el-table-column>
-                        <el-table-column prop="total_production_in_number" label="累记生产入库数量" align="center"></el-table-column>
-                        <el-table-column prop="damage_number" label="损坏数量" align="center"></el-table-column>
-                        <el-table-column prop="summary_time" label="汇总时间" align="center"></el-table-column>
-                    </el-table>
-                </el-tabs>
+                <TableProxy :url="mainurl" :param="mainparam" :reload="dataTableReload"   :page-size="20" >
+                    <el-table-column label="序号" align="center" type="index" width="65"></el-table-column>
+                    <el-table-column prop="entrepot.name" label="配送中心" width="180" align="center"></el-table-column>
+                    <el-table-column prop="goods_name" label="商品名称" width="180" align="center"></el-table-column>
+                    <el-table-column prop="goods.cate_kind" label="商品种类" width="180" align="center"></el-table-column>
+                    <el-table-column prop="saleable_count" label="可用库存数" width="180" align="center"></el-table-column>
+                    <el-table-column prop="entrepot_out" label="累记出库数量" align="center"></el-table-column>
+                    <el-table-column prop="entrepot_id" label="累记生产入库数量" align="center"></el-table-column>
+                    <el-table-column prop="bad_num" label="损坏数量" align="center"></el-table-column>
+                    <!-- <el-table-column prop="summary_time" label="汇总时间" align="center"></el-table-column> -->
+                </TableProxy>
             </el-col>
         </el-row>
     </div>
@@ -69,20 +81,33 @@
     import PageMix from '../../mix/Page';
     import SearchTool from '../../mix/SearchTool';
     import DataTable from '../../mix/DataTable';
+    import DistributionCenterSelectProxy from '@/packages/DistributionCenterSelectProxy';
+    import CategorySelectProxy from '../../packages/CategorySelectProxy';
+
+    import InventoryGather from '@/ajaxProxy/InventoryGather';
+
     export default {
         name:'StockSum',
         pageTitle:"库存汇总",
         mixins: [PageMix, SearchTool,DataTable],
         data(){
             return {
+                ajaxProxy:InventoryGather,
+                mainurl:InventoryGather,
+                mainparam:"",
                 searchForm: {
+                    entrepot_id: '',
+                    cate_type_id:"",
+                    cate_kind_id:"",
+
                     product_id: "",
                     goods_type_id: '',
                     goods_name: '',
                     storage_id: '',
                     goodsShelveNumber: '',
-                    distribution_id: '',
                     searchTime:'',
+                    start:"",
+                    end:""
                 },
                 dataList:[
                     {   distribution_name:"圆通",
@@ -104,10 +129,8 @@
                     },
 
                 ],
-                types: [
-                    {id:1,name:'面膜'},
-                    {id:2,name:'爽肤水'},
-                ],
+                types: [],
+                cate_kinds:[],
                 productNames: [
                     {id:1,name:'面膜 6张'},
                     {id:2,name:'爽肤水 200ml'},
@@ -116,14 +139,47 @@
                     {id:1,name:'张三'},
                     {id:2,name:'李四'},
                 ],
-                distributors: [
-                    {id:1,name:'顺丰'},
-                    {id:2,name:'圆通'},
-                ],
+                distributors:[]
+            }
+        },
+        methods:{
+            onSearchChange(param){
+                if (this.searchForm.searchTime.length!=2) {
+                    param.start = "";
+                    param.end = "";
+                }
+                this.mainparam = JSON.stringify(param);
+            },
+            loadEntrepot(data){
+                this.distributors = data.items;
+            },
+            getTypes(data){
+                this.types = data.items;
+            },
+            typeChange(v){
+                for (let index = 0; index < this.types.length; index++) {
+                    const element = this.types[index];
+                    if (element.id == v) {
+                        this.cate_kinds = element.children;
+                    }
+                }
+            },
+            timeChange(v){
+                if (v && v.length > 1) {
+                    let d = v.split(" - ");
+                    this.searchForm.start = d[0]+ " 00:00:00";
+                    this.searchForm.end = d[1]+ " 23:59:59";
+                }
             }
         },
         created(){
             this.$on('search-tool-change', this.onSearchChange);
+
+            this.entrepotSelect =  new DistributionCenterSelectProxy({}, this.loadEntrepot, this);
+            this.entrepotSelect.load();
+
+            let CategorySelect = new CategorySelectProxy({}, this.getTypes, this);
+            CategorySelect.load();
         }
     }
 </script>

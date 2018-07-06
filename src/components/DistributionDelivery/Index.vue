45a8ed6fca3d3b4dd5489eca873a409c5cd0ffec
mcdyqnc <hyfdyx@qq.com>
2018-07-06 18:52:07 +0800

    菜鸟 api  print

--- src/components/DistributionDelivery/Index.vue
+++ src/components/DistributionDelivery/Index.vue
@@ -135,8 +135,7 @@
         <!-- /table -->
         <br>
         <!-- 下部展示组件 -->
-        <SubDetail :row="model">
-        </SubDetail>
+        <SubDetail :row="model"></SubDetail>
         <!-- 写弹窗组件 -->
         <edit-address name='edit-address'
             :ajax-proxy="ajaxProxy"
@@ -148,12 +147,12 @@
         <RepeatOrder name="repeat-order" :ajax-proxy="ajaxProxy" @submit-success="handleReload"></RepeatOrder>
         <StopOrder name="stop-order" :ajax-proxy="ajaxProxy"  @submit-success="handleReload"></StopOrder>
 
-        <!-- <el-button @click="printList">获取打印机列表</el-button>
+        <el-button @click="printList">获取打印机列表</el-button>
         <el-button @click="configprint">弹窗式配置打印机</el-button>
         <el-button @click="getPrinterConfig">Fax打印机的配置</el-button>
         <el-button @click="previewPrint">打印预览PDF</el-button>
-        <el-button @click="staticPrint">打印静态数据</el-button> -->
-    </div>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+        <el-button @click="staticPrint">打印静态数据</el-button>
+    </div>
 </template>
 <script>
 import PageMix from '@/mix/Page';
@@ -372,8 +371,8 @@ export default {
 
                 //         }).catch(() => {
                                 
-                        });
-                }
+                //         });
+                // }
                 // todo 
                 // this.$modal.show('express', param);
                 //与打印机通讯 要面单什么的
@@ -401,7 +400,6 @@ export default {
         },
         showAssign(){
             if (this.openDialogCheck()) {
-                
                 let pr = true;
                 if (this.currentRow.assign_print_status == 1) {
                     pr = false;
@@ -420,10 +418,7 @@ export default {
                         }).catch(() => {
                                 
                         });
-                }
-                // todo 
-                // this.$modal.show('express', param);
-                //与打印机通讯 要面单什么的
+                }     
                 if (pr) {
                     AssignAjaxProxy.goodsPrint(this.currentRow.id).then((response)=>{
                         //获取打印的数据　估计还要设置一下格式
@@ -434,6 +429,66 @@ export default {
                     });
                     
                 }
+            }
+        },
+        /**
+     * 
+     *  {
+            "data": {
+                "tabletest": [
+                    {
+                        "name": "测试商品",
+                        "goods_number": "1"
+                    },
+                    {
+                        "name": "测试商品",
+                        "goods_number": "1"
+                    },
+                    {
+                        "name": "测试商品",
+                        "count": "1"
+                    }
+                ],
+                "address": {
+                    "name": "zzz",
+                    "phone": "bbb"
+                },
+                "assign_sn": "bbbbaaaaa",
+                "total": "12",
+                entrepot:""
+            },
+            "signature": "19d6f7759487e556ddcdd3d499af087080403277b7deed1a951cc3d9a93c42a7e22ccba94ff609976c5d3ceb069b641f541bc9906098438d362cae002dfd823a8654b2b4f655e96317d7f60eef1372bb983a4e3174cc8d321668c49068071eaea873071ed683dd24810e51afc0bc925b7a2445fdbc2034cdffb12cb4719ca6b7",
+            "templateURL": "http://cloudprint.cainiao.com/print/resource/getResource.json?resourceId=3139575&status=0"
+        }
+     * 
+     * 
+     * 
+     */
+        getPrintGoodsListData(goodsList, row){
+            let tabletest = [];
+            let total = 0;
+            for (let index = 0; index < goodsList.length; index++) {
+                const element = goodsList[index];
+                let tmp = {
+                    name:"["+ element.goods_number +"]" + "个  "+ element.sku_sn + "  " + element.goods_name + "  " + element.specifications ,
+                    goods_number: element.goods_number 
+                };
+                tabletest.push(tmp);
+                total += parseInt(element.goods_number);
+            }
+
+            let entrepot = this.distributors.find(function(value){
+                return value.id = row.entrepot_id;
+            })
+            return {
+                data:{
+                    tabletest:tabletest,
+                    address:row.address,
+                    assign_sn:row.assign_sn,
+                    total:total,
+                    entrepot:entrepot.name
+                },
+                templateURL:"http://cloudprint.cainiao.com/template/standard/251026"
             }
         },
         printList(){

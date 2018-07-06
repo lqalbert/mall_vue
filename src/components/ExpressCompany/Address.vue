ece8501dc07dce5812c75edfd36591a6e99c0f57
mcdyqnc <hyfdyx@qq.com>
2018-07-06 18:52:07 +0800

    菜鸟 api  print

--- src/components/ExpressCompany/Address.vue
+++ src/components/ExpressCompany/Address.vue
@@ -1,119 +1,149 @@
 <template>
+    <!-- 本来是设置地址的，后来发现查询剩余的面单号也是用这个 -->
         <div>
-            <MyDialog title="选择地址" :name="name" :width="width" :height="height" @before-open="onOpen">
+            <MyDialog title="订购查询" :name="name" :width="width" :height="height" @before-open="onOpen" @before-close="onBeforeClose">
                 <el-form :model="editForm" ref="editForm" :label-width="labelWidth"   :label-position="labelPosition">
-                    <el-alert
+                    <!-- <el-alert
                         title="请选择正确的地址 "
                         type="warning"
                         show-icon>
-                      </el-alert>
-                    <el-row>
-                        <el-col :span="12">
-                            <el-form-item prop="send_address"  label="地址">
-                                <el-radio-group v-model="editForm.send_address">
-                                    <el-radio v-for="item in addresslist" :label="item.address" :key="item.id">{{ item.name }} - {{item.address.province}} - {{item.address.town}} - {{item.address.city}} - {{item.address.district}} - {{item.address.detail}} </el-radio>
-                                </el-radio-group>
-                            </el-form-item>
-                        </el-col>
-                        
-                    </el-row>
+                    </el-alert> -->
+                    <div v-for="bill in waybill">
+                        <el-row>
+                            <el-col :span="24">
+                                cpCode: {{ cpCode }} 
+                                <el-alert
+                                    title="当没有设置cpCode时 菜鸟接口会返回一个默认的cpCode，仔细核对这个cpCode是不是正确的。"
+                                    type="warning"
+                                    show-icon>
+                                </el-alert>
+                            </el-col>
+                        </el-row>
+                        <el-row >
+                            <el-col :span="12">
+                                <el-form-item label="已用面单数量">
+                                    {{ bill.allocatedQuantity }} 
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="12">
+                                <el-form-item label="取消面单总数">
+                                    {{ bill.cancelQuantity }}   
+                                </el-form-item>
+                            </el-col>
+                        </el-row>
+
+                        <el-row>
+                            <el-col :span="12">
+                                <el-form-item label="已经打印面单总数">
+                                    {{ bill.printQuantity }}      
+                                </el-form-item>
+                            </el-col>
+                            <el-col :span="12">
+                                <el-form-item label="面单余额数量">
+                                    {{ bill.quantity }}         
+                                </el-form-item>
+                            </el-col>
+                        </el-row>
+
+                        <el-row>
+                            <el-col :span="24">
+                                <el-form-item prop="send_address"  label="地址">
+                                    <el-radio-group v-model="editForm.send_address">
+                                        <el-radio v-for="(item, addressIndex) in bill.shippAddressCols" :label="item" :key="addressIndex">{{item.province}}  - {{item.city}} - {{item.town}} - {{item.district}} - {{item.detail}} </el-radio>
+                                    </el-radio-group>
+                                </el-form-item>
+                            </el-col>
+                        </el-row>
+                    </div>
+
+                    
                 </el-form>
                 <div slot="dialog-foot" class="dialog-footer">
+                    <el-button @click="getCainiao" :loading="queryIng">查 询</el-button> 
                     <el-button @click="handleClose">取 消</el-button>
                     <submit-button
                             :observer="dialogThis"
                             @click="formSubmit('editForm')">
-                        保 存
+                        保存地址
                     </submit-button>
                 </div>
             </MyDialog>
         </div>
-    </template>
-    <script>
-    import DialogForm from '@/mix/DialogForm';
-    import APP_CONST from '@/config';
-    import { mapGetters } from 'vuex';
-    
-    export default {
-        name: 'Edit',
-        mixins:[DialogForm],
-        props:{
-            ajaxProxy:{type: Object}
-        },
-        data(){
-            return {
-                dialogThis:this,
-                labelPosition:"right",
-                labelWidth:'80px',
-                editForm:{
-                    id:'',
-                    send_address:null
-                },
-                addresslist:[]
-            }
-        },
-        methods:{
-            getAjaxPromise(model){
-                return this.ajaxProxy.updateAddress(model.id, model);
+</template>
+<script>
+import DialogForm from '@/mix/DialogForm';
+import APP_CONST from '@/config';
+import { mapGetters } from 'vuex';
+
+export default {
+    name: 'Edit',
+    mixins:[DialogForm],
+    props:{
+        ajaxProxy:{type: Object}
+    },
+    data(){
+        return {
+            dialogThis:this,
+            labelPosition:"right",
+            labelWidth:'80px',
+            editForm:{
+                id:'',
+                send_address:null
             },
-            onOpen(param){
-                this.editForm.id = param.params.id;    
-                this.ajaxProxy.getAddress(param.params.id).then((Response)=>{
-                    
-                    if (Response.data.state ==1) {
-                        this.parseAddress(Response.data.data['waybillApplySubscriptionCols']);
-                    } else {
-                        this.$message.error(Response.data.msg);
-                    }
-                    
-                }).catch((Response)=>{
-                    this.$message.error(Response.data.msg);
-                });
-            },
-
-            parseAddress(ar){
-                let re = [];
-                
-                if (ar.length == 0) {
-                    this.$message.error('请联系管理员，没有找到发货地址');
-                    return ;
-                }
-
-                for (let index = 0; index < ar.length; index++) {
-                    const element = ar[index];
-                    var d = {};
-                    if (element.branchAccountCols.length) {
-                        d.address = element.branchAccountCols[0].shippAddressCols[0];
-                        d.name    = element.branchAccountCols[0].branchName;
-                    }
-
-                    re.push(d);   
-                }
-
-
-                this.addresslist = re;
-            }
-            
-        },
-        created(){
-            // this.addresslist = [{
-            //         address:{
-            //             "province":"浙江省",
-            //             "town":"仓前街道",
-            //             "city":"杭州市",
-            //             "district":"余杭区",
-            //             "detail":"文一西路"
-            //         },
-            //         name:'杭州网点'
-            //     }];
-        },
-        
-    
-    }
-    </script>
-    <style scoped>
-        .name-input{
-            max-width: 217px;
+            waybill:[],
+            cpCode:"",
+            queryIng:false
         }
-    </style>
+    },
+    methods:{
+        getAjaxPromise(model){
+            return this.ajaxProxy.updateAddress(model.id, model);
+        },
+        onOpen(param){
+            this.editForm.id = param.params.id;       
+        },
+        parseAddress(ar){
+            let re = [];
+            if (ar.length == 0) {
+                this.$message.error('请联系管理员，没有找到发货地址');
+                return ;
+            }
+            for (let index = 0; index < ar.length; index++) {
+                const element = ar[index];
+                var d = {};
+                if (element.branchAccountCols.length) {
+                    d.address = element.branchAccountCols[0].shippAddressCols[0];
+                    d.name    = element.branchAccountCols[0].branchName;
+                }
+                re.push(d);   
+            }
+            this.addresslist = re;
+        },
+        getCainiao(){
+            this.queryIng = true;
+            this.ajaxProxy.getAddress(this.editForm.id).then((Response)=>{
+                this.queryIng = false;
+                if (Response.data.status == 1) {
+                    this.waybill = Response.data.data[0].branchAccountCols;
+                    this.cpCode = Response.data.data[0].cpCode;
+                } else {
+                    this.$message.error(Response.data.msg);
+                }
+            }).catch((Response)=>{
+                this.queryIng.false;
+                this.$message.error('操作失败');
+            });
+        },
+        onBeforeClose(){
+            this.waybill = [];
+            this.cpCode = "";
+        }
+    }
+}
+</script>
+<style scoped>
+    .name-input{
+        max-width: 217px;
+    }
+</style>
     

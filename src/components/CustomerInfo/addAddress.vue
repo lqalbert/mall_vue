bc4b8225a57fb87ecce0dc9dd56e06f668194be5
hujianjian <hujian_gosu@163.com>
2018-07-17 17:27:10 +0800

    201807171727 fix bug

--- src/components/CustomerInfo/addAddress.vue
+++ src/components/CustomerInfo/addAddress.vue
@@ -171,6 +171,7 @@ export default {
                 fixed_telephone:[
                     // pattern: /^1[34578]\d{9}$/
                     { required: true, message:'请输入正确的号码', trigger:'blur'},
+                    // { required: true, message:'请输入正确的座机号', pattern: /^0\d{2,3}-\d{7,8}$/,trigger:'blur'},
                 ],
                 address:[
                     { required: true,message:'请输入收货地址',  trigger:'blur'},

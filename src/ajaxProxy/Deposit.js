b024b6f2bfb61cc3444257914b691fe59724cd0f
hujianjian <hujian_gosu@163.com>
2018-07-20 14:21:11 +0800

    20180720121保证金撤销

--- src/ajaxProxy/Deposit.js
+++ src/ajaxProxy/Deposit.js
@@ -3,4 +3,8 @@ import RestfulProxy from './RestfulProxy';
 const URL = '/deposits';
 const proxy = new RestfulProxy(URL);
 
+proxy.revoke = function (id,model) {
+    return this.getAxios().put("/deposit-revoke/" + id,model);
+}
+
 export default proxy;

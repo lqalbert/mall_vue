f4367cbb93e427403b1bd45e63a2f14d064c9941
mcdyqnc <hyfdyx@qq.com>
2018-07-23 10:56:58 +0800

    运费模板

---
+++ src/components/FreightTemplate/Edit.vue
@@ -0,0 +1,174 @@
+<template>
+    <div>
+        <MyDialog title="编辑" :name="name" :width="width" :height="height" @before-open="onOpen">
+            <el-form :model="editForm"  ref="editForm" :rules="rules" :label-width="labelWidth"   :label-position="labelPosition">
+                <el-row>
+                    <el-col :span="12">
+                        <el-form-item label="名称" prop="name" >
+                            <el-input class="name-input"  placeholder="" v-model="editForm.name"></el-input>
+                        </el-form-item>
+                    </el-col>
+
+                    <el-col :span="12">
+                        <el-form-item label="配送中心" prop="entrepot_id">
+                            <el-select v-model.number="editForm.entrepot_id" >
+                                <el-option v-for="(v,index) in entrepots"
+                                    :label="v.name" 
+                                    :value="v.id"
+                                    :key="v.id">
+                                </el-option>
+                            </el-select>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+                <el-row>
+                    <el-col :span="12">
+                        <el-form-item label="快递公司" prop="express" >
+                            <el-input class="name-input"  placeholder="" v-model="editForm.express"></el-input>
+                        </el-form-item>
+                    </el-col>
+                    
+                </el-row>
+                <el-row>
+                    <el-col :span="12">
+                        <el-form-item label="是否统一运费" prop="is_unify" >
+                            <el-radio class="radio" v-model="editForm.is_unify" :label="0">否</el-radio>
+                            <el-radio class="radio" v-model="editForm.is_unify" :label="1">是</el-radio>
+                        </el-form-item>
+                    </el-col>
+                    <el-col :span="8" v-show="isUnify">
+                        <el-form-item label="统一运费" prop="unify_fee" >
+                            <el-input v-model="editForm.unify_fee"></el-input>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+
+                <div v-show="!isUnify">
+                    <el-row>
+                        <el-col :span="18">
+                            <el-form-item label="支持包邮吗" prop="is_include" >
+                                <el-radio class="radio" v-model="editForm.is_include" :label="0">否</el-radio>
+                                <el-radio class="radio" v-model="editForm.is_include" :label="1">是</el-radio>
+                                <span>&nbsp;&nbsp;&nbsp;如果不支持包邮，则在包邮时不能选择该快递</span>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+
+                    <el-row v-show="isInclude">
+                        <el-col :span="12">
+                            <el-form-item label="满足包邮金额" prop="stand_fee" >
+                                <el-input class="name-input"  placeholder="" v-model="editForm.stand_fee"><template slot="append">(含)</template></el-input>
+                            </el-form-item>
+                        </el-col>
+                        <el-col :span="12">
+                            <el-form-item label="包邮额外费用" prop="stand_extra" >
+                                <el-input class="name-input"  placeholder="" v-model="editForm.stand_extra"></el-input>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+
+                    <el-row >
+                        <el-col :span="18">
+                            <el-form-item label="不包邮基本费用" prop="basic_fee" >
+                                <el-input class="name-input"  placeholder="" v-model="editForm.basic_fee"></el-input>
+                                <span>特殊地区另外存储</span>
+                            </el-form-item>
+                        </el-col>
+                    </el-row>
+                </div>
+            </el-form>
+    
+            <div slot="dialog-foot" class="dialog-footer">
+                <el-button @click="handleClose">取 消</el-button>
+                <submit-button
+                        @click="formSubmit('editForm')"
+                        :observer="dialogThis">
+                    保 存
+                </submit-button>
+            </div>
+
+        </MyDialog>
+    </div>
+</template>
+
+<script>
+import DialogForm from '../../mix/DialogForm';
+
+import { mapGetters } from 'vuex';
+
+export default {
+    name: 'Add',
+    mixins:[DialogForm],
+    data () {
+        return {
+            dialogThis:this,
+            labelPosition:"right",
+            labelWidth:'100px',
+           
+           
+            editForm:{
+                id:'',
+                entrepot_id:"",
+                name:"",
+                express:"默认",
+                is_unify:0,
+                is_include:0,
+                unify_fee:"0.00",
+                stand_fee:"268.00",
+                stand_extra:"0.00",
+                basic_fee:"10.00"
+            },
+            rules:{
+                name:[
+                    { required: true, message:"请输入模板名", type:'string'}
+                ],
+                entrepot_id:[
+                    { required: true, message:"请选择配送中心", type:'number'}
+                ]
+            },
+
+            model:{}
+            
+        }
+    },
+    computed:{
+        ...mapGetters({
+            'entrepots':'getEntrepots'
+        }),
+        isInclude(){
+            return this.editForm.is_include == '1';
+        },
+        isUnify(){
+            return this.editForm.is_unify == '1';
+        }
+    },
+    methods:{
+        getAjaxPromise(model){
+            return this.ajaxProxy.update(this.model.id, model);
+        },
+        onOpen(param){
+            this.model = param.params.model;
+        }
+    },
+    watch:{
+        model:function(val, oldVal){
+            for (const key in this.editForm) {
+                if (this.editForm.hasOwnProperty(key)) {
+                    this.editForm[key] = val[key]
+                }
+            }
+        }
+    },
+    created(){
+
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+    .name-input{
+        max-width: 217px;
+    }
+</style>
+      
--- src/components/FreightTemplate/SubDetail.vue
+++ src/components/FreightTemplate/SubDetail.vue
@@ -3,13 +3,26 @@
         <el-table :data="tableData"  style="width: 100%">
             <!-- 仿编辑订单 商品明细那里处理 -->
             <el-table-column   prop="province.name"   label="省份"  width="180">
+                <template slot-scope="scope">
+                    <span v-if="!scope.row.editState">{{ scope.row.province.name }}</span>
+                    <span v-else><el-select size="small" v-model.number="scope.row.province_id">
+                        <el-option v-for="item in provinces" :label="item.name" :value="item.id" :key="item.id"></el-option>
+                    </el-select></span>
+                </template>
             </el-table-column>
             <el-table-column prop="fee"  label="费用"  width="180">
+                <template slot-scope="scope">
+                    <span v-if="!scope.row.editState">{{ scope.row.fee }}</span>
+                    <span v-else>
+                        <el-input size="small" v-model="scope.row.fee"></el-input>
+                    </span>
+                </template>
             </el-table-column>
             <el-table-column  label="操作">
                 <template slot-scope="scope">
-                    <el-button size="small">编辑</el-button>
-                    <el-button size="small">删除</el-button>
+                    <el-button size="small" @click="triggerEdit(scope.row)" v-if="!scope.row.editState">编辑</el-button>
+                    <el-button size="small" @click="save(scope.row)" v-else>保存</el-button>
+                    <el-button size="small" @click="deleteItem(scope.row.id)" type="danger">删除</el-button>
                 </template>
             </el-table-column>
         </el-table>
@@ -18,6 +31,8 @@
       
 <script>
     import FreightExtraProxy from '@/packages/FreightExtraProxy';
+    import FreightExtraAjax from '@/ajaxProxy/FreightExtra';
+    import { mapGetters } from 'vuex';
     export default {
         name: 'FreightExtra',
         props:{
@@ -30,9 +45,56 @@
                 tableData:[]
             }
         },
+        computed:{
+            
+            ...mapGetters({
+                'provinces':'province'
+            }),
+        },
         methods:{
             loadData(data){
+                data.items.forEach(element => {
+                    element.editState = false;
+                });
                 this.tableData = data.items;
+            },
+            triggerEdit(row){
+                row.editState = true;
+            },
+            save(row){
+                let model = {
+                    province_id: row.province_id,
+                    fee: row.fee
+                }
+                FreightExtraAjax.update(row.id, model).then((response)=>{
+                    if (response.data.status == 0) {
+                        this.$message.error(response.data.msg);
+                    } else {
+                        this.$message.success(response.data.msg);
+                    }
+                    row.editState = false;
+                }).catch((response)=>{
+
+                })
+            },
+            deleteItem(id){
+                let vmthis = this;
+                this.$confirm('是否继续?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    FreightExtraAjax.delete(id).then((response)=>{
+                        vmthis.freiprxoy.setParam({
+                            fr_id: vmthis.currentRow.id,
+                            with:['province']
+                        }).load();
+                    }).catch((response)=>{
+                        vmthis.$message.error(response.data.msg);
+                    })
+                }).catch(() => {
+                          
+                });
             }
         },
         watch:{

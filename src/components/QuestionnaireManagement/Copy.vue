3aab8523cd8e0b4e0722f702e265d51c498370bd
yrg <huj@riign.com>
2018-08-21 15:05:14 +0800

    20180821问卷管理跳转修改

--- src/components/QuestionnaireManagement/Copy.vue
+++ src/components/QuestionnaireManagement/Copy.vue
@@ -4,37 +4,13 @@
                 <el-form :model="editForm" :rules="rules" :label-width="labelWidth"  ref="editForm" :label-position="labelPosition">
                     <el-row>
                         <el-col :span="16">
-                            <el-form-item label="测试服务器访问网址" prop="title" >
+                            <el-form-item label="访问网址" prop="title" >
                                 <el-input   v-text="title" auto-complete="off" ></el-input>
                             </el-form-item>
                         </el-col>
                         <el-col :span="4">
                             <el-form-item>
-                                <el-button type="primary" size="small" @click="goHtml('title')">跳转到页面</el-button>
-                            </el-form-item>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col :span="16">
-                            <el-form-item label="开发访问网址" prop="title1" >
-                                <el-input  v-text="title1"  auto-complete="off" ></el-input>
-                            </el-form-item>
-                        </el-col>
-                        <el-col :span="4">
-                            <el-form-item>
-                                <el-button type="primary" size="small" @click="goHtml('title1')">跳转到页面</el-button>
-                            </el-form-item>
-                        </el-col>
-                    </el-row>
-                    <el-row>
-                        <el-col :span="16">
-                            <el-form-item label="正式服务器访问网址" prop="title2" >
-                                <el-input  v-text="title2"  auto-complete="off" ></el-input>
-                            </el-form-item>
-                        </el-col>
-                        <el-col :span="4">
-                            <el-form-item>
-                                <el-button type="primary" size="small" @click="goHtml('title2')">跳转到页面</el-button>
+                                <el-button type="primary" size="small" @click="goHtml()">跳转到页面</el-button>
                             </el-form-item>
                         </el-col>
                     </el-row>
@@ -54,6 +30,7 @@
     <script>
         import DialogForm from '../../mix/DialogForm';
         import APP_CONST from '../../config';
+        import {QUESTIONNAIRE_URL} from '../../config';
         import { quillRedefine } from 'vue-quill-editor-upload';
         export default {
             name: 'Copy',
@@ -84,19 +61,8 @@
                 onOpen(param){
                     this.model = param.params.model;
                 },
-                goHtml(name){
-                    switch (name){
-                        case 'title':
-                            window.open(this.title);
-                            break;
-                        case 'title1':
-                            window.open(this.title1);
-                            break;
-                        case 'title2':
-                            window.open(this.title2);
-                            break;
-                    }
-
+                goHtml(){
+                    window.open(this.title);
                 },
                 getAjaxPromise(model){
                     return this.ajaxProxy.update(model.id, model);
@@ -104,9 +70,9 @@
             },
             watch:{
               model:function (val,oldval) {
-                  this.title = "http://yanggang112.51vip.biz:50009/questionnaire" + '?id=' + val.id;
-                  this.title1 = "http://localhost:8000/questionnaire" + '?id=' + val.id;
-                  this.title2 = "http://pulata.com.cn/questionnaire" + '?id=' + val.id;
+                  // this.title = "http://yanggang112.51vip.biz:50009/questionnaire" + '?id=' + val.id;
+                  this.title = QUESTIONNAIRE_URL + val.id;
+                  // this.title2 = "http://pulata.com.cn/questionnaire" + '?id=' + val.id;
               }
             },
             created(){

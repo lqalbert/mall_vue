5de5b7e8ffd48e305dc1e3c121668ceca6d3e1da
mcdyqnc <hyfdyx@qq.com>
2018-09-06 14:46:29 +0800

    aa fix

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import store from './store'
 
 
 import ElementUI from 'element-ui'
-// import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-default/index.css'
 
 import VModal from 'vue-js-modal'
 import VueQuillEditor from 'vue-quill-editor'

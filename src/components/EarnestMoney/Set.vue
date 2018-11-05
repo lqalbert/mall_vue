10e3b9d8ff923358161bb14bfd663b1bb6c291cd
mcdyqnc <hyfdyx@qq.com>
2018-11-05 17:42:29 +0800

    未完成

--- src/components/EarnestMoney/Set.vue
+++ src/components/EarnestMoney/Set.vue
@@ -15,34 +15,72 @@
                         </el-form-item>
                     </el-col>
                 </el-row>
-
+                
                 <el-row>
                     <el-col :span="12">
-                        <el-form-item label="赠品扣除比例" prop="appendage_rate" >
-                            <el-input placeholder="请输入内容" v-model="addForm.appendage_rate">
+                        <el-form-item label="运营比例YK" prop="yk" >
+                            <el-input placeholder="请输入内容" v-model="addForm.yk">
                                 <template slot="append">%</template>
                             </el-input>
                         </el-form-item>
                     </el-col>
 
                     <el-col :span="12">
-                        <el-form-item label="正常销售扣除比例" prop="sale_rate" >
-                            <el-input placeholder="请输入内容" v-model="addForm.sale_rate">
-                                <template slot="append">%</template>
-                            </el-input>
-                        </el-form-item>
-                    </el-col>
-                </el-row>
-                <el-row>
-                    <el-col :span="12">
-                        <el-form-item label="返还比例" prop="return_rate">
-                            <el-input placeholder="请输入内容" v-model="addForm.return_rate">
+                        <el-form-item label="运营比例赠品YZ" prop="yz" >
+                            <el-input placeholder="请输入内容" v-model="addForm.yz">
                                 <template slot="append">%</template>
                             </el-input>
                         </el-form-item>
                     </el-col>
                 </el-row>
 
+                <el-row>
+                    <el-col :span="12">
+                        <el-form-item label="仓库扣除C" prop="c" >
+                            <el-input placeholder="请输入内容" v-model="addForm.c">
+                                <template slot="append">%</template>
+                            </el-input>
+                        </el-form-item>
+                    </el-col>
+
+                    <el-col :span="12">
+                        <el-form-item label="赠品折扣ZN" prop="zn" >
+                            <el-input placeholder="请输入内容" v-model="addForm.zn">
+                                <template slot="append">%</template>
+                            </el-input>
+                        </el-form-item>
+                    </el-col>
+
+                </el-row>
+
+                <el-row>
+                    <el-col :span="12">
+                        <el-form-item label="京东扣点J" prop="j" >
+                            <el-input placeholder="请输入内容" v-model="addForm.j">
+                                <template slot="append">%</template>
+                            </el-input>
+                        </el-form-item>
+                    </el-col>
+
+                    <el-col :span="12">
+                        <el-form-item label="运营承担Y" prop="y" >
+                            <el-input placeholder="请输入内容" v-model="addForm.y">
+                                <template slot="append">%</template>
+                            </el-input>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+
+                <el-row>
+                    <el-col :span="12">
+                        <el-form-item label="内购折扣N" prop="n" >
+                            <el-input placeholder="请输入内容" v-model="addForm.n">
+                                <template slot="append">%</template>
+                            </el-input>
+                        </el-form-item>
+                    </el-col>
+                </el-row>
+                
             </el-form>
     
             <div slot="dialog-foot" class="dialog-footer">
@@ -79,9 +117,13 @@ export default {
             
             addForm:{
                 type:"",
-                appendage_rate:"",
-                sale_rate:"",
-                return_rate:""
+                yk:"",
+                yz:"",
+                c:"",
+                zn:"",
+                j:"",
+                y:"",
+                n:""
             },
             rules:{
                 // money:[
@@ -109,9 +151,16 @@ export default {
             this.ajaxProxy.getSet().then((response)=>{
                 const model = response.data.data;
                 this.addForm.type = model.type;
-                this.addForm.appendage_rate = model.appendage_rate;
-                this.addForm.sale_rate = model.sale_rate;
-                this.addForm.return_rate = model.return_rate;
+                this.addForm.yk = model.yk;
+                this.addForm.yz = model.yz;
+                this.addForm.c = model.c;
+                this.addForm.zn = model.zn;
+                this.addForm.j = model.j;
+                this.addForm.y = model.y;
+                this.addForm.n = model.n;
+                // this.addForm.appendage_rate = model.appendage_rate;
+                // this.addForm.sale_rate = model.sale_rate;
+                // this.addForm.return_rate = model.return_rate;
             });
         } 
     },
